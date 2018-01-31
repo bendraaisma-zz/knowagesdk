753faef290a49fa357fbadebdc53e8c756ecda74
Bernard Arjan Draaisma <bendraaisma@gmail.com>
2018-01-31 13:20:23 -0200

    1.0.4

--- index.js
+++ index.js
@@ -1,38 +1,33 @@
 'use strict'
 
-const cometdJquery = require("cometd-jquery");
 const sbisdk = require("./sbisdk-all-production");
 
-exports.setBaseUrl = function(config) {
-	sbisdk.sdk.services.setBaseUrl(config);
+module.exports = {
+	setBaseUrl : function(config) {
+		sbisdk.sdk.services.setBaseUrl(config);
+	},
+	getDocumentHtml : function(config) {
+		return sbisdk.sdk.api.getDocumentHtml(config);
+	},
+	injectDocument : function(config) {
+		return sbisdk.sdk.api.injectDocument(config);
+	},
+	getWorksheetHtml : function(config) {
+		return sbisdk.sdk.api.getWorksheetHtml(config);
+	},
+	injectWorksheet : function(config) {
+		return sbisdk.sdk.api.injectWorksheet(config);
+	},
+	getQbeHtml : function(config) {
+		return sbisdk.sdk.api.getQbeHtml(config);
+	},
+	injectQbe : function(config) {
+		return sbisdk.sdk.api.injectQbe(config);
+	},
+	getDataSetList : function(config) {
+		return sbisdk.sdk.api.getDataSetList(config);
+	},
+	executeDataSet : function(config) {
+		return sbisdk.sdk.api.executeDataSet(config);
+	}
 }
-
-//module.exports = {
-//	setBaseUrl : function(config) {
-//		sbisdk.sdk.services.setBaseUrl(config);
-//	},
-//	getDocumentHtml : function(config) {
-//		return sbisdk.sdk.api.getDocumentHtml(config);
-//	},
-//	injectDocument : function(config) {
-//		return sbisdk.sdk.api.injectDocument(config);
-//	},
-//	getWorksheetHtml : function(config) {
-//		return sbisdk.sdk.api.getWorksheetHtml(config);
-//	},
-//	injectWorksheet : function(config) {
-//		return sbisdk.sdk.api.injectWorksheet(config);
-//	},
-//	getQbeHtml : function(config) {
-//		return sbisdk.sdk.api.getQbeHtml(config);
-//	},
-//	injectQbe : function(config) {
-//		return sbisdk.sdk.api.injectQbe(config);
-//	},
-//	getDataSetList : function(config) {
-//		return sbisdk.sdk.api.getDataSetList(config);
-//	},
-//	executeDataSet : function(config) {
-//		return sbisdk.sdk.api.executeDataSet(config);
-//	}
-//}
