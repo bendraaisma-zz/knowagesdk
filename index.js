55d0de475a1b1c400c7c6d71ed7bf7431b941075
Bernard Arjan Draaisma <bendraaisma@gmail.com>
2018-01-31 10:47:25 -0200

    Init commit of module

---
+++ index.js
@@ -0,0 +1,32 @@
+var cometdJquery = require("cometd-jquery");
+var sbisdk = require("./sbisdk-all-production");
+
+module.exports = {
+	setBaseUrl : function(config) {
+		sbisdk.sdk.services.setBaseUrl(config);
+	},
+	getDocumentHtml : function(config) {
+		return sbisdk.sdk.api.getDocumentHtml(config);
+	},
+	injectDocument : function(config) {
+		return sbisdk.sdk.api.injectDocument(config);
+	},
+	getWorksheetHtml : function(config) {
+		return sbisdk.sdk.api.getWorksheetHtml(config);
+	},
+	injectWorksheet : function(config) {
+		return sbisdk.sdk.api.injectWorksheet(config);
+	},
+	getQbeHtml : function(config) {
+		return sbisdk.sdk.api.getQbeHtml(config);
+	},
+	injectQbe : function(config) {
+		return sbisdk.sdk.api.injectQbe(config);
+	},
+	getDataSetList : function(config) {
+		return sbisdk.sdk.api.getDataSetList(config);
+	},
+	executeDataSet : function(config) {
+		return sbisdk.sdk.api.executeDataSet(config);
+	}
+}
