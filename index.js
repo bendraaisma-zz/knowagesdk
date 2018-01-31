cc46b9f3512fbddd95fc8acf5514ed0620d85f9f
Bernard Arjan Draaisma <bendraaisma@gmail.com>
2018-01-31 21:07:11 -0200

    1.0.12

--- index.js
+++ index.js
@@ -4,89 +4,49 @@ const sbisdk = require("./sbisdk-all-production");
 
 var sbiModule = (function() {
 
-	var baseUrlModule = (function() {
-
-		var BaseUrl = function() {
-			this.protocol = 'http';
-			this.host = 'localhost';
-			this.port = '8080';
-			this.contextPath = 'knowage';
-			this.controllerPath = 'servlet/AdapterHTTP';
-		}
-
-		return {
-			BaseUrl : BaseUrl
-		}
-
-	}());
-
-	var iFrameModule = (function() {
-
-		var Iframe = function() {
-			this.height = '400px';
-			this.width = '100%';
-			this.style = 'border: 0px;';
-		}
-
-		return {
-			Iframe : Iframe
-		}
-
-	}());
-	
-	var documentHtmlConfigModule = (function() {
-
-		var DocumentHtmlConfig = function() {
-			this.documentId = '';
-			this.documentLabel = '';
-			this.parameters = null;
-			this.executionRole = '';
-			this.displayToolbar = false;
-			this.displaySliders = false;
-			this.iframe = new iFrameModule();
-		}
-
-		return {
-			DocumentHtmlConfig : DocumentHtmlConfig
-		}
-
-	}());
-	
-	var Sbi = function(config = new baseUrlModule.BaseUrl()) {
+	var Sbi = function(config) {
 		sbisdk.sdk.services.setBaseUrl(config);
 	};
+	
+	Sbi.prototype.authenticate = function(param, callback) {
+		return sbisdk.sdk.api.authenticate(param, callback);
+	};
+	
+	Sbi.prototype.authenticate = function(credentials, headers, callback) {
+		return sbisdk.sdk.api.authenticate(credentials, headers, callback);
+	};
 
-	Sbi.prototype.getDocumentHtml = function(config = new documentHtmlConfigModule.DocumentHtmlConfig()) {
+	Sbi.prototype.getDocumentHtml = function(config) {
 		return sbisdk.sdk.api.getDocumentHtml(config);
 	};
 
-//	Sbi.prototype.injectDocument = function(configModule) {
-//		return sbisdk.sdk.api.injectDocument(configModule);
-//	};
-//
-//	Sbi.prototype.getWorksheetHtml = function(configModule) {
-//		return sbisdk.sdk.api.getWorksheetHtml(configModule);
-//	};
-//
-//	Sbi.prototype.injectWorksheet = function(configModule) {
-//		return sbisdk.sdk.api.injectWorksheet(configModule);
-//	};
-//
-//	Sbi.prototype.getQbeHtml = function(configModule) {
-//		return sbisdk.sdk.api.getQbeHtml(configModule);
-//	};
-//
-//	Sbi.prototype.injectQbe = function(configModule) {
-//		return sbisdk.sdk.api.injectQbe(configModule);
-//	};
-//
-//	Sbi.prototype.getDataSetList = function(configModule) {
-//		return sbisdk.sdk.api.getDataSetList(configModule);
-//	};
-//
-//	Sbi.prototype.executeDataSet = function(configModule) {
-//		return sbisdk.sdk.api.executeDataSet(configModule);
-//	};
+	Sbi.prototype.injectDocument = function(configModule) {
+		return sbisdk.sdk.api.injectDocument(configModule);
+	};
+
+	Sbi.prototype.getWorksheetHtml = function(configModule) {
+		return sbisdk.sdk.api.getWorksheetHtml(configModule);
+	};
+
+	Sbi.prototype.injectWorksheet = function(configModule) {
+		return sbisdk.sdk.api.injectWorksheet(configModule);
+	};
+
+	Sbi.prototype.getQbeHtml = function(configModule) {
+		return sbisdk.sdk.api.getQbeHtml(configModule);
+	};
+
+	Sbi.prototype.injectQbe = function(configModule) {
+		return sbisdk.sdk.api.injectQbe(configModule);
+	};
+
+	Sbi.prototype.getDataSetList = function(configModule) {
+		return sbisdk.sdk.api.getDataSetList(configModule);
+	};
+
+	Sbi.prototype.executeDataSet = function(configModule) {
+		return sbisdk.sdk.api.executeDataSet(configModule);
+	};
 
 	return {
 		Sbi : Sbi
